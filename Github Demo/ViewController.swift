b80e509a6ae339192c59a06e82749a58d0a9f065
Christopher Knapp <knappce@bc.edu>
2020-09-03 16:31:58 -0400

    Test for SF state

--- Github Demo/ViewController.swift
+++ Github Demo/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-print("hello")
+    
+    func print("sf state") {
+        
+    }
 
 }
 
